b348300e918665baa05327010ffb8c8d01d80a03
Belle Nguyen <Belle@Belles-MBP-3.fios-router.home>
2017-12-30 19:27:10 -0800

    modified condition statements

--- tdee.js
+++ tdee.js
@@ -3,28 +3,31 @@ function computeTDEE(){
     var height = document.getElementById('height').value * 2.54;
     var age = document.getElementById('age').value;
     if (document.getElementById('t-units').value === 'lb'){
-        weight = (weight * 0.453592)
+        weight = (weight * 0.453592) //convert lb to kg
+     } else if (document.getElementById('t-units').value === 'select'){
+        document.getElementById("TDEE").innerHTML = "";
      }
+
     if (document.getElementById('t-sex').value === 'female'){
         c=-161
-    }else{
+    } else if (document.getElementById('t-sex').value === 'male'){
         c=5
-
+    } else {
+        document.getElementById("TDEE").innerHTML = "";
     }
+
     if (document.getElementById('lifestyle').value === 'sedentary'){
         a=1.2
-    }
-    if (document.getElementById('lifestyle').value === 'lightly'){
+    } else if (document.getElementById('lifestyle').value === 'lightly'){
         a=1.375
-    }
-    if (document.getElementById('lifestyle').value === 'moderate'){
+    } else if (document.getElementById('lifestyle').value === 'moderate'){
         a=1.55
-    }
-    if (document.getElementById('lifestyle').value === 'very'){
+    } else if (document.getElementById('lifestyle').value === 'very'){
         a=1.725
-    }
-    if (document.getElementById('lifestyle').value === 'extremely'){
+    } else if (document.getElementById('lifestyle').value === 'extremely'){
         a=1.9
+    } else {
+        document.getElementById("TDEE").innerHTML = "";
     }
 
     var TDEE = (((10*weight) + (6.25*height) - (5*age) + c)*a).toFixed(0);
