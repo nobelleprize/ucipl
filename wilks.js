fb2d879e852416abb9df370caaa10d3bc7e54386
Belle Nguyen <Belle@Belles-MBP-3.fios-router.home>
2017-12-30 19:28:32 -0800

    modified condition statements

--- wilks.js
+++ wilks.js
@@ -4,6 +4,8 @@ function computeWilks(){
      if (document.getElementById('units').value === 'lb'){
           weight = (weight * 0.453592)
           total = (total * 0.453592)
+       } if (document.getElementById('units').value === 'select'){
+           document.getElementById("wilks").innerHTML = "";
        }
      if (document.getElementById('sex').value === 'female'){
         a = 594.31747775582;
@@ -12,13 +14,15 @@ function computeWilks(){
         d = -0.00930733913;
         e = 4.731582e-05;
         f = -9.054e-08;
-      }else{
+      } else if (document.getElementById('sex').value === 'male'){
         a = -216.0475144;
         b = 16.2606339;
         c = -0.002388645;
         d = -0.00113732;
         e = 7.01863e-06;
         f = -1.291E-08;
+      } else {
+        document.getElementById("wilks").innerHTML = "";
      }
     var coeff = (500) / ((a) + (b * weight) + (c * Math.pow(weight, 2)) + (d * Math.pow(weight, 3)) + (e * Math.pow(weight, 4)) + (f * Math.pow(weight, 5)));
     var wilks = (coeff * total).toFixed(2);
